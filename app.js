2c430ab406fe7f0bfbceb5f497880038a9a40541
JungYoungSick <junfas1216@gmail.com>
2024-01-17 00:43:19 +0900

    강제주입교육 재시작

--- app.js
+++
@@ -1,7 +0,0 @@
-let Number = 13;
-
-if(Number %2 === 0){
-  console.log("짝수")
-} else {
-  console.log("홀수")
-}
--- app.py
+++
@@ -1,6 +0,0 @@
-Number = 16;
-
-if Number %2 == 0:
-  print("짝수")
-else:
-  print("홀수")
